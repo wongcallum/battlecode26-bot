ffa58b193b4a9bad021fe537453833392cc49faa
Callum <mail@callumwong.com>
2026-06-24 03:14:49 +1000

    scale king spawn count to nearby mines instead of a flat cap

--- src/wongcallum/RatKing.java
+++ src/wongcallum/RatKing.java
@@ -12,7 +12,10 @@ public final class RatKing {
     private RatKing() {
     }
 
-    private static final int TARGET_RATS = 12;
+    // have a range of rats scaled to income to improve performance on maps with low cheese
+    private static final int RATS_MIN = 4;
+    private static final int RATS_MAX = 14;
+    private static final int RATS_PER_MINE = 2;
     private static final int CHEESE_BUFFER = 400;
     private static final int SPAWN_INTERVAL = 8;
     private static final int CAT_TRAP_RANGE_DSQ = 16; // only trap a cat closing on us
@@ -39,7 +42,7 @@ public final class RatKing {
                 && rc.getNumberCatTraps() < TrapType.CAT_TRAP.maxCount
                 && cheese >= CHEESE_BUFFER + TrapType.CAT_TRAP.buildCost
                 && placeCatTrapToward(rc, cat.getLocation())) {
-        } else if (spawnedCount < TARGET_RATS
+        } else if (spawnedCount < Math.min(RATS_MAX, RATS_MIN + RATS_PER_MINE * Comms.knownMines)
                 && round - lastSpawnRound >= SPAWN_INTERVAL
                 && cheese >= rc.getCurrentRatCost() + CHEESE_BUFFER) {
             // don't rely on visible rats, the king spawns too many on big maps
