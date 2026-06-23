afe62bc7dd45da1717b8cde6968aa1a7b5dbe548
Callum <mail@callumwong.com>
2026-06-24 01:14:42 +1000

    cap spawning with a total count for now

--- src/wongcallum/RatKing.java
+++ src/wongcallum/RatKing.java
@@ -10,27 +10,29 @@ public final class RatKing {
     private RatKing() {
     }
 
-    private static final int TARGET_RATS = 10;
+    private static final int TARGET_RATS = 12;
     private static final int CHEESE_BUFFER = 400;
     private static final int SPAWN_INTERVAL = 8;
 
     static int lastSpawnRound = -SPAWN_INTERVAL;
+    static int spawnedCount = 0;                     // persistent total spawned
 
     static void act(RobotController rc) throws GameActionException {
         int cheese = rc.getAllCheese();
         int round = rc.getRoundNum();
-        rc.setIndicatorString("RAT_KING | hp=" + rc.getHealth() + " cheese=" + cheese);
+        rc.setIndicatorString("RAT_KING | hp=" + rc.getHealth() + " cheese=" + cheese + " spawned=" + spawnedCount);
 
-        // try to sustainably spawn by having a rate limit and a minimum buffer
-        int liveRats = Utils.visibleAlliedRats(rc);
-        if (round - lastSpawnRound >= SPAWN_INTERVAL
-                && liveRats < TARGET_RATS
+        // on big maps, if we only count the visible maps then the king will overspawn
+        // the rate limit and buffer make sure we dont spawn all at once
+        if (spawnedCount < TARGET_RATS
+                && round - lastSpawnRound >= SPAWN_INTERVAL
                 && cheese >= rc.getCurrentRatCost() + CHEESE_BUFFER) {
             // King is 3x3
             for (Direction d : Utils.directions) {
                 MapLocation loc = rc.getLocation().translate(2 * d.getDeltaX(), 2 * d.getDeltaY());
                 if (rc.canBuildRat(loc)) {
                     rc.buildRat(loc);
+                    spawnedCount += 1;
                     lastSpawnRound = round;
                     break;
                 }
